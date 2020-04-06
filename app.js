dae081f171b3da437ba4f889653eb496e319ee2a
Cameron Scott <camscottgreen@hotmail.com>
2020-04-06 16:36:33 +1000

    Add routes and controllers Complete tutorial part 4

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
 
 require('dotenv').config()
 
@@ -14,7 +15,7 @@ var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
 var mongoDB = process.env.DB_STRING;
-mongoose.connect(mongoDB, { userNewUrlParser: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
