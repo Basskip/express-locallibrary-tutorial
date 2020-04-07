6f0b45dc456510c5dad36c56ace0393b45b0df3f
Cameron Scott <camscottgreen@hotmail.com>
2020-04-07 14:38:32 +1000

    Complete tutorial part 5 Add all read-only pages

--- models/author.js
+++ models/author.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+var moment = require('moment');
 var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema(
@@ -37,6 +37,15 @@ AuthorSchema
     return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
   });
 
+AuthorSchema
+.virtual('lifespan_string')
+.get(function () {
+  let str = '';
+  str += this.date_of_birth ? moment(this.date_of_birth).format('MMMM Do, YYYY') : '';
+  str += ' - '
+  str += this.date_of_death ? moment(this.date_of_death).format('MMMM Do, YYYY') : '';
+  return str;
+})
 // Virtual for author's URL
 AuthorSchema
   .virtual('url')
