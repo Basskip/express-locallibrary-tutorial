346f63331b1e6e8584ef76053b643da3854fb9b5
Cameron Scott <camscottgreen@hotmail.com>
2020-04-10 11:46:00 +1000

    Prepare for heroku deployment

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+var compression = require('compression');
+var helmet = require('helmet');
 
 require('dotenv').config()
 
 var app = express();
+
+app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
@@ -27,6 +31,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
